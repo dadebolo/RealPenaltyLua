4cacbb4bf43444f37328052f5b8cdd64ff14832f
dadebolo <53077855+dadebolo@users.noreply.github.com>
2023-12-21 18:27:55 +0100

    Update ZeroGas.lua

--- ZeroGas.lua
+++ ZeroGas.lua
@@ -1,11 +1,10 @@
-local server_irt = '123.321.23.32'
-
+local socket_listening = false
 function script.update(dt)
-    sim = ac.getSim()
-    if sim.isOnlineRace then
-        if ac.getServerIP() ~= server_irt then
-            ac.accessCarPhysics().gas=0
-        end
-    end
-
-  
+    ac.log("zerogas.lua 0000")
+    if socket_listening then
+    else
+        ac.log("zerogas.lua 0001")
+--    ac.accessCarPhysics().gas = 0
+    ac.log("zerogas.lua 0002")
+    ac.log("zerogas.lua 9999")
+end
