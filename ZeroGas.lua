8f23a8920ccc0356cad0c5d167fc631e9c687c44
dadebolo <53077855+dadebolo@users.noreply.github.com>
2023-12-21 18:31:29 +0100

    Update ZeroGas.lua

--- ZeroGas.lua
+++ ZeroGas.lua
@@ -1,11 +1,11 @@
-local socket_listening = false
+--local socket_listening = false
 function script.update(dt)
     ac.log("zerogas.lua 0000")
-    if socket_listening then
-    else
-        ac.log("zerogas.lua 0001")
+--    if socket_listening then
+--    else
+--        ac.log("zerogas.lua 0001")
 --    ac.accessCarPhysics().gas = 0
-    end        
+--    end        
     ac.log("zerogas.lua 0002")
     ac.log("zerogas.lua 9999")
 end
